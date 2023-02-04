d768da4a607865aec9fe4c4d1b34abdddb2989e9
Bamboozle-jpg <kevin@bachelorclan.com>
2023-02-04 11:24:24 -0800

    v2 setup complete

---
+++ src/page.svelte
@@ -0,0 +1,31 @@
+<script>
+	import Box from './responderAlert.svelte';
+</script>
+
+<style>
+	h1 {
+		color: #930000;
+		font-family: 'Noto Sans', sans-serif;
+		font-size: 2em;
+        text-align: center;
+        padding-bottom: 15px;
+        padding-top: 15px;
+        background-color: #F2FEFF;
+	}
+    body {
+        background-color: #F2FEFF;
+        padding: 20px;
+        width:auto;
+        height:auto;
+        position:auto;
+    }
+</style>
+<body>
+    <h1>Requests for Help</h1>
+    <Box/>
+    <Box/>
+    <Box/>
+    <Box/><br>
+    <Box/><br>
+    <Box/><br>
+</body>
