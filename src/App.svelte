802603a7691ca485f475f6ce0c41efabe6c03134
Bamboozle-jpg <kevin@bachelorclan.com>
2023-02-04 01:32:59 -0800

    Revert "1 file modified, 1 added"
    This reverts commit a23a8cca6a221c32e8f3cba794dc1a389d9a4517.

--- src/App.svelte
+++ src/App.svelte
@@ -3,22 +3,21 @@
 </script>
 
 <main>
-	<h1>SlugAlert</h1>
+	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
 <style>
-  @import url('https://fonts.googleapis.com/css2?family=Rubik:wght@600&display=swap');
 	main {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-		font-family: 'Rubik', sans-serif;
-		color: #00575F;
-}
+	}
+
 	h1 {
-		color: #00575F;
+		color: #ff3e00;
+		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
 	}
