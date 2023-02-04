4779060fa5e2f145eb3ab4582c71ac7b4923e407
Bamboozle-jpg <kevin@bachelorclan.com>
2023-02-04 01:12:44 -0800

    Begin firebase setup

---
+++ responder-page/src/routes/auth/login.svelte
@@ -0,0 +1,34 @@
+<script>
+    import SignIn from "../../lib/auth/sign_in.svelte";
+    import { Link } from "carbon-components-svelte";
+</script>
+<div>
+    <div class="header">
+        <h4>Login</h4>
+    </div>
+    <div class="signin-form">
+        <SignIn />
+        <div>Already have an account? <Link href="/auth/register">Sign Up</Link></div>
+    </div>
+</div>
+<style>
+     .header {
+         width: 100vw;
+         padding: 2em 0;
+         min-height: 20vh;
+         display: flex;
+         justify-content: center;
+         align-items: center;
+         background-color: #E5F0FF;
+     }
+     .header h4 {
+         color: black;
+         font-weight: 600;
+         font-size: 3rem;
+     }
+     .signin-form {
+         min-height: 80vh;
+         display: grid;
+         place-items: center;
+     }
+</style>
---
+++ responder-page/src/routes/auth/register.svelte
@@ -0,0 +1,34 @@
+<script>
+    import SignUp from "../../lib/auth/sign_up.svelte";
+    import { Link } from "carbon-components-svelte";
+</script>
+<div>
+    <div class="header">
+        <h4>Sign Up</h4>
+    </div>
+    <div class="form-container">
+        <SignUp/>
+        <div>Already have an account? <Link href="/auth/login">Sign In</Link></div>
+    </div>
+</div>
+<style>
+    .header {
+         width: 100vw;
+         padding: 2em 0;
+         min-height: 20vh;
+         display: flex;
+         justify-content: center;
+         align-items: center;
+         background-color: #E5F0FF;
+     }
+     .header h4 {
+         color: black;
+         font-weight: 600;
+         font-size: 3rem;
+     }
+     .form-container {
+         min-height: 80vh;
+         display: grid;
+         place-items: center;
+     }
+</style>
