da1189994a3a52b0e6fb7368749c3a4ee4d2e2f2
Bamboozle-jpg <kevin@bachelorclan.com>
2023-02-05 02:08:36 -0800

    touch upp App

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,12 @@
     let landing = true;
     let sHome = false; // student home
     let rHome = false; // responder home
+    let rMap = false;
 
     import Landing from "./screens/Landing.svelte";
     import ResponderPage from "./screens/ResponderPage.svelte";
     import Student from "./screens/Student.svelte";
+    import MapHolder from "./screens/mapHolder.svelte";
 
 </script>
 
@@ -15,10 +17,10 @@
         <Landing />
     {:else if sHome}
         <Student />
-
     {:else if rHome}
         <ResponderPage />
-
+    {:else if rMap}
+        <MapHolder />
     {/if}
     
 </body>
