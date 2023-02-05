726b08b765b32e71db20f0b25e4f38c28cd6ca65
Harshita Bhardwaj <honeyblue2604@gmail.com>
2023-02-04 17:19:05 -0800

    Added user type options

--- src/Landing.svelte
+++ src/Landing.svelte
@@ -1,12 +1,38 @@
 <script>
-	import LongButton from "./components/LongButton.svelte";
+	import Button from "./components/Button.svelte";
     import InputField from "./components/InputField.svelte";
+
+    let sClick = false;
+    let marginT= "0.5em";
+
+    const sClicked = (id) => { 
+        sClick = !sClick;
+        if(rClick) rClick = !rClick; 
+        console.log(id)
+    };
+    let rClick = false
+    const rClicked = (id) => { 
+        rClick = !rClick; 
+        if(sClick) sClick = !sClick; 
+        console.log(id)
+    };
+
+    let userBtns = [
+        {btnTxt: "Student",
+        id:"student", 
+        clickEvent:sClicked,
+        clicked:sClick},
+        {btnTxt: "Responder",
+        id:"responder", 
+        clickEvent:rClicked,
+        clicked:rClick},
+    ];
 
 </script>
 
 <main style="display:flex; flex-direction:column;">
     <!-- App Name -->
-    <div class="row" style="margin-bottom: 5em;">
+    <div class="row">
         <h1>Slug Alert</h1>
         <img 
             src="./img/slug.png" 
@@ -27,8 +53,20 @@
             fieldName="Phone Number"
             fieldID="userPhoneNumber"
             placeholder="(000)-111-2345"/>
+
+        <label for="userType">User Type</label>
+        <div class="row" style="justify-content: left; gap: 2em">
+            {#each userBtns as btn (btn.id)}
+                <Button 
+                    btnTxt={btn.btnTxt}
+                    clicked={btn.clicked}
+                    on:click={() => btn.clickEvent(btn.id)}
+                    {marginT}/>
+
+            {/each}
+        </div>
     
-        <LongButton btnTxt="Continue" outline={true} id="logInBtn"/>
+        <Button btnTxt="Continue" classType={"outline"} id="logInBtn"/>
     </div>
     <!-- <div id="btns">
         <LongButton btnTxt="Log In" grad={true} id="logInBtn"/> <br>
@@ -51,13 +89,14 @@
         flex-direction: row;
         justify-content: center;
         align-items: center;
+        height: fit-content;
     }
 
     #userInfo {
         display: flex;
         flex-direction: column;
         justify-content: center;
-        /* height: 100%; */
+        height: 70%;
     }
 
 </style>
